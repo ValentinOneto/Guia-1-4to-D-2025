8a1d8bfa0fa2be58541445da27ada12a9cfbeb1e
ValentinOneto <49261732@est.ort.edu.ar>
2025-03-31 10:31:58 -0300

    ejercicios hechos

---
+++ Assets/Scripts/datosDelUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class datosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float alturaUsuario;
+    public bool esDonante;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Mi nombre es {nombreUsuario}, tengo {edadUsuario} años, mido {alturaUsuario}cm y es {esDonante} que soy donante de sangre.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
