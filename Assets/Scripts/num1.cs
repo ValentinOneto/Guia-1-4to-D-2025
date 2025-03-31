2adaca567a9d2765a081bca66936316eb2c086dd
ValentinOneto <49261732@est.ort.edu.ar>
2025-03-31 10:43:23 -0300

    ejercicio 1 guia 2 empezado

---
+++ Assets/Scripts/num1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class num1 : MonoBehaviour
+
+{
+    int n1;
+    int resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"El resultado de la suma de {n1} + 2 es igual a {n1 + 2}" );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
