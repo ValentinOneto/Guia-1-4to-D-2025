722a6626afe548d656c96b57dab092f03c95eba6
ValentinOneto <49261732@est.ort.edu.ar>
2025-04-03 10:16:06 -0300

    ejercicio 6 completado

---
+++ Assets/Scripts/parOImpar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parOImpar : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 > 0)
+        {
+            if(num1 % 2 == 0)
+            {
+                Debug.Log($"El numero {num1} es par.");
+            }
+            else
+            {
+                Debug.Log($"El numero {num1} es impar.");
+            }
+        }
+        else
+        {
+            Debug.Log("El numero debe ser mayor que cero");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
