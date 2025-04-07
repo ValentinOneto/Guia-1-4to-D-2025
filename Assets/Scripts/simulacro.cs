4af112a90907f8a2cda65e1713eb2b8598084139
ValentinOneto <49261732@est.ort.edu.ar>
2025-04-07 10:27:40 -0300

    t1 simulacro de prueba completado

---
+++ Assets/Scripts/simulacro.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simulacro : MonoBehaviour
+{
+    public string moneda;
+    public float cantidad;
+    float cotizacionMonedaElegida;
+    float montoConvertido;
+    float montoMinimo = 1000;
+    float cotizacionDolar = 1000;
+    float cotizacionEuro = 1200;
+    float cotizacionReal = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1 dolar: 1000 pesos, 1 real: 200 pesos, 1 euro: 1200
+
+        moneda = moneda.ToLower();
+    if (cantidad < montoMinimo)
+        {
+            Debug.Log($"El monto mínimo es $1000");
+            return;
+        }
+
+        if (moneda == "d")
+        {
+            cotizacionMonedaElegida = cotizacionDolar;
+        }
+    else if (moneda == "r")
+        {
+            cotizacionMonedaElegida = cotizacionReal;
+        }
+    else if (moneda == "e")
+            {
+                cotizacionMonedaElegida = cotizacionEuro;
+            }
+    
+    else
+        {
+            Debug.Log($"Opción de moneda extranjera no válida");
+            return;
+        }
+
+        montoConvertido = cantidad / cotizacionMonedaElegida;
+        Debug.Log($"Tu monto convertido es: {montoConvertido}");
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
