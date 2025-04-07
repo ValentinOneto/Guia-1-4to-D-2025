10b15c392a6126b86a964ca56ff8aa2cb9988bd9
ValentinOneto <49261732@est.ort.edu.ar>
2025-04-07 10:40:49 -0300

    t3 del simulacro empezado

---
+++ Assets/Scripts/refugioAnimales.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class refugioAnimales : MonoBehaviour
+{
+    float alimentoGato = 300;
+    float alimentoPerroPequeno = 400;
+    float alimentoPerroGrande = 700;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
